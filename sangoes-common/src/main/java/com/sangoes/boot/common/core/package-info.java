1b21fc21320be2b82fca523da41b2a9e04e954fa
jerrychir <jerrychir@163.com>
2018-10-27 23:55:49 +0800

    swagger

---
+++ sangoes-common/src/main/java/com/sangoes/boot/common/core/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Copyright (c) 2018
+ *
+ * @author jerrychir
+ * @date 2018/10/27 11:23 PM
+ */
+package com.sangoes.boot.common.core;
--- sangoes-uc/src/main/java/com/sangoes/boot/uc/UCApplication.java
+++ sangoes-uc/src/main/java/com/sangoes/boot/uc/UCApplication.java
@@ -1,5 +1,6 @@
 package com.sangoes.boot.uc;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -11,6 +12,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  * @date 2018/10/27 8:06 PM
  */
 @SpringBootApplication
+@EnableSwagger2Doc
 @MapperScan(basePackages = "tk.mybatis.springboot.mapper")
 public class UCApplication {
     public static void main(String[] args) {
